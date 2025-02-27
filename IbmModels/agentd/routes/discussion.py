77a569a08ea9ed7a853ea9a3754605b5462ffd54
GhaouiYoussef <youssef.ghaoui@ensi-uma.tn>
2025-02-27 03:25:23 +0100

    Enhance agents discussion functionality with user company parameter and logging; add test for discussion route

--- IbmModels/agentd/routes/discussion.py
+++ IbmModels/agentd/routes/discussion.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter, HTTPException
 from ibm_watson import generate_response
 from database import discussions_collection
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-def agent_interaction(WebSiteSummary: str, UserCompany: str = 'AI Consulting') -> list:
+def agent_interaction(WebSiteSummary: str, UserCompany: str) -> list:
     """Handles a structured competitive debate between two agents—one for and one against working with the client."""
     
     conversation = []
@@ -54,21 +59,26 @@ def agent_interaction(WebSiteSummary: str, UserCompany: str = 'AI Consulting') -
     return conversation
 
 @router.get("/agents-discussion/")
-async def agents_discussion(WebSiteSummary: str = None):
+async def agents_discussion(WebSiteSummary: str = None, UserCompany: str = 'AI Consulting', test: bool = False):
     """Handles a structured agent discussion and returns a summary."""
     try:
-        conversation = agent_interaction(WebSiteSummary)
 
-        summary_prompt = f"""
-        You are Judge Agent analyzing the debate. You must provide an argumented final decision to either proceed or not.
-        Debate:
-        {conversation}
-        """
+        if not test:
+            conversation = agent_interaction(WebSiteSummary, UserCompany)
+        else:
+            conversation = WebSiteSummary
+            summary_prompt = f"""
+            You are Senior Strategic Advisor Agent analyzing the debate of two junior Agents. You must provide an argumented final decision in 150 word max, to either proceed or not with this client.
+            The debate:
+            {conversation}
+            """
         
-        summary = generate_response(summary_prompt, model_id='ibm/granite-13b-instruct-v2', max_tokens=100)
-
+        summary = generate_response(summary_prompt, max_tokens=300)
+        # print("Summary:", summary.replace(summary, ''))
         # Extract "FINAL DECISION"
         final_decision_start = summary.upper().find("FINAL DECISION:")
+        # logging.info(f"FINAL DECISION start: {final_decision_start}")
+        
         summary = summary[final_decision_start:].strip() if final_decision_start != -1 else "FINAL DECISION: Unable to determine."
 
         # Store in MongoDB
